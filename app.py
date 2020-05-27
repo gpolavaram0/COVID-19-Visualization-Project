efe0b72ea9163f218c56059517d1b344aca6d6ca
gpolavaram0 <60547556+gpolavaram0@users.noreply.github.com>
2020-05-26 20:18:24 -0400

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template
 
 
 #################################################

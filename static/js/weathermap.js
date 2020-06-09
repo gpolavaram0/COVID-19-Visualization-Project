952a9cc19c9818917a2e13a82034af03e2a4f851
gpolavaram0 <60547556+gpolavaram0@users.noreply.github.com>
2020-06-09 19:39:28 -0400

    Add files via upload

--- static/js/weathermap.js
+++ static/js/weathermap.js
@@ -14,17 +14,38 @@ const airbaseLayer = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/t
 const myMap = L.map("weather-heatmap", {
     center: [39.50, -98.35],
     zoom: 4,
-    layers: [airbaseLayer]
+    layers: [baseLayer, SO2Layer]
 });
+
+//Create initial heatmap layers
+//Set array for each parameter
+let COArr = [];
+let O3Arr = [];
+let NO2Arr = [];
+let SO2Arr = [];
+//Create initial heatmap layers
+let COLayer = L.heatLayer(COArr, {radius:50, blur:30});
+let O3Layer = L.heatLayer(O3Arr, {radius:50, blur:30});
+let NO2Layer = L.heatLayer(NO2Arr, {radius:50, blur:30});
+let SO2Layer = L.heatLayer(SO2Arr, {radius:50, blur:30});
+//Set Overlay Layers
+const airoverlayMaps = {
+    "Sulfur Dioxide (ppb)": SO2Layer,
+    "Ozone (ppm)": O3Layer,
+    "Carbon Monoxide (ppm)": COLayer,
+    "Nitrogen Dioxide (ppb)": NO2Layer,
+};
+//Create Layer control
+L.control.layers(airoverlayMaps).addTo(myMap);
 
 //Function to run code
 function runAir() {
     //Set date value to a variable
     let airDateValue = airDate.property("value");
+    console.log(airDateValue);
 
     //Read in csv data
     d3.json(`https://covid19bootcampproject3.herokuapp.com/air_quality/${airDateValue}`, airData => {
-
         //Parse through data
         airData.forEach(d => {
             d.observation_count = +d.observation_count
@@ -36,64 +57,27 @@ function runAir() {
         const NO2 = airData.filter(d => d.parameter === "Nitrogen dioxide (NO2)");
         const SO2 = airData.filter(d => d.parameter === "Sulfur dioxide");
 
-        //Create initial heatmap layers
-        //Set array for each parameter
-        let COArr = [];
-        let O3Arr = [];
-        let NO2Arr = [];
-        let SO2Arr = [];
         //Add lat lngs to the arrays
         CO.forEach(d => {COArr.push([d.latitude, d.longitude, d.observation_count])});
         O3.forEach(d => {O3Arr.push([d.latitude, d.longitude, d.observation_count])});
         NO2.forEach(d => {NO2Arr.push([d.latitude, d.longitude, d.observation_count])});
         SO2.forEach(d => {SO2Arr.push([d.latitude, d.longitude, d.observation_count])});
 
-        //Create initial heatmap layers
-        let COLayer = L.heatLayer(COArr, {radius:50, blur:30});
-        let O3Layer = L.heatLayer(O3Arr, {radius:50, blur:30});
-        let NO2Layer = L.heatLayer(NO2Arr, {radius:50, blur:30});
-        let SO2Layer = L.heatLayer(SO2Arr, {radius:50, blur:30});
-
-        //Set default layer
-        myMap.layers.push(SO2Layer);
-
-        //Set Overlay Layers
-        let overlayMaps = {
-            "Sulfur Dioxide (ppb)": SO2Layer,
-            "Ozone (ppm)": O3Layer,
-            "Carbon Monoxide (ppm)": COLayer,
-            "Nitrogen Dioxide (ppb)": NO2Layer,
-        };
-
-        function renderAir() {
-            //Grab date value
-            airDateValue = airDate.property("value");
-            console.log(airDateValue);
-            //Reset arrays
-            COArr = [];
-            O3Arr = [];
-            NO2Arr = [];
-            SO2Arr = [];
-            //Repush data
-            CO.forEach(d => {COArr.push([d.latitude, d.longitude, d.observation_count])});
-            O3.forEach(d => {O3Arr.push([d.latitude, d.longitude, d.observation_count])});
-            NO2.forEach(d => {NO2Arr.push([d.latitude, d.longitude, d.observation_count])});
-            SO2.forEach(d => {SO2Arr.push([d.latitude, d.longitude, d.observation_count])});
-            //Reset layers
-            COLayer.setLatLngs(COArr);
-            O3Layer.setLatLngs(O3Arr);
-            NO2Layer.setLatLngs(NO2Arr);
-            SO2Layer.setLatLngs(SO2Layer);
-        }
-
-        //Create Layer control
-        L.control.layers(overlayMaps).addTo(myMap);
-
-        //Add legend to map
-        legendAir.addTo(myMap);
-
-        //Event handler to update heatmap
-        airDate.on("change.air", renderAir);
+        //Reset arrays
+        COArr = [];
+        O3Arr = [];
+        NO2Arr = [];
+        SO2Arr = [];
+        //Repush data
+        CO.forEach(d => {COArr.push([d.latitude, d.longitude, d.observation_count])});
+        O3.forEach(d => {O3Arr.push([d.latitude, d.longitude, d.observation_count])});
+        NO2.forEach(d => {NO2Arr.push([d.latitude, d.longitude, d.observation_count])});
+        SO2.forEach(d => {SO2Arr.push([d.latitude, d.longitude, d.observation_count])});
+        //Reset layers
+        COLayer.setLatLngs(COArr);
+        O3Layer.setLatLngs(O3Arr);
+        NO2Layer.setLatLngs(NO2Arr);
+        SO2Layer.setLatLngs(SO2Layer);
     });
 }
 //Create legend for the map
@@ -116,5 +100,9 @@ legendAir.onAdd = function (map) {
     return div;
 } 
 
+//Add legend to map
+legendAir.addTo(myMap);
+
 //Event handler to run code
 airDateType.on("change.air", runAir);
+airDate.on("change.air", runAir);

61bd9417b51777c934839bdc6ccd9144c32aea27
gpolavaram0 <60547556+gpolavaram0@users.noreply.github.com>
2020-06-13 20:58:25 -0400

    Update app.py

--- app.py
+++ app.py
@@ -13,28 +13,11 @@ import get_data
 
 import psycopg2
 
-from boto.s3.connection import S3Connection
-# s3 = S3Connection(os.environ['S3_KEY'], os.environ['S3_SECRET'])
-#################################################
-# Database Setup
-#################################################
-# # engine = create_engine("")
-# Base = automap_base()
-# Base.prepare(engine, reflect=True)
-# Base.classes.keys()
-# Station = Base.classes.station
-# Measurement = Base.classes.measurement
-# county = Base.classes.county_clean
-
-# State = Base.classes.state
-
-# session = Session(engine)
 #################################################
 # Flask Setup
 #################################################
 app = Flask(__name__)
 
-###############################
 app._static_folder = 'static/'
 
 # load the tickers we want into a dataframe
@@ -46,18 +29,17 @@ tickers = pd.DataFrame({
 
 ##################################
 
-# DATABASE_URL will contain the database connection string:
+# DATABASE_URL will contain the database connection string
 
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://hdjdaacogqimcu:a6007ea2abde788e2b86e856357cb8741377410b135800ea087bd2780f50e2fb@ec2-52-44-55-63.compute-1.amazonaws.com:5432/dbh8e6jsnrlr1k'
 
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://postgres:Eagle2fox1!project2@project2db.cvkw7pcnugqv.us-east-2.rds.amazonaws.com:5432/project2db'
-
+# SQLALCHEMY_DATABASE_URI_AWS links to AWS database
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ["SQLALCHEMY_DATABASE_URI_AWS"]
 
 
 # # Connects to the database using the app config
 db = SQLAlchemy(app)
 
+#function to reflect db, store in dataframe, and convert to a format that the javascript files can read
 def database_csv_retriever(csv_name, *args):
     csv_name_db = db.Table(csv_name, db.metadata, autoload = True, autoload_with = db.engine)
     csv_query = db.session.query(csv_name_db).all()
@@ -71,67 +53,18 @@ def database_csv_retriever(csv_name, *args):
 
     return csv_lod
 
-########################################################
-
-# csv_name_db = db.Table('infection_date', db.metadata, autoload = True, autoload_with = db.engine)
-# csv_query = db.session.query(csv_name_db).filter(csv_name_db.c.date_local == "2020-01-22").all()
-# print(csv_query)
-# csv_date_df = pd.DataFrame(csv_query, columns = ['date_local','cases','deaths'])
-# csv_date_df = csv_date_df.reset_index(drop=True)
-# csv_date_df['date_local'] = csv_date_df['date_local'].astype(str)
-# csv_date_df2 = csv_date_df.loc[csv_date_df['date_local'] == '2020-01-22', ['date_local','cases','deaths']]
-# print(csv_date_df2)
-
-# stock_data = get_data.main()
-# print(stock_data)
-# stock_data_df = pd.DataFrame(stock_data)
-
-# stock_data_df.drop(index = 0, inplace = True)
-# stock_data_df['date'] = stock_data_df['date'].astype(str)
-# stock_data_df=stock_data_df.rename(columns = {'date':'date_local'})
-    
-# stock_data_lod = stock_data_df.to_dict('records')
-
-##########################################################
-# database_csv_retriever("infection_date",'date','cases','deaths')
-
-# csv_name_db = db.Table("air_line", db.metadata, autoload = True, autoload_with = db.engine)
-# csv_query = db.session.query(csv_name_db).column_descriptions()
-
-# for title in csv_query:
-#     print(title)
-
-# x = db.session.query(infection_date).all()
-# print(x)
-# session.query(Invoices.BillingCountry).group_by(Invoices.BillingCountry).all()
-
-
-# data = engine.execute("SELECT * FROM test_data_csv;")
-# print(x)
-
-# results = session.query(Station.name)
-
 #################################################
 # Flask Routes
 #################################################
 
 @app.route("/")
 def welcome():
-    # """List all available api routes."""
-    # return (
-    #     f"Available Routes:<br/>"
-    #     f"/api/v1.0/names<br/>"
-    #     f"/api/v1.0/passengers"
-    # )
 
     return render_template("index.html")
-
 
 @app.route("/test")
 def test():
    
-
-    # results = session.query(county.county).all()
     x = db.session.query(test_data).all()
     test_dict = {"key":"value"}
     print(x)
@@ -167,14 +100,6 @@ def county_clean(county_clean_single_date):
 @app.route("/infection_date")
 def infection_date():
     infection_date_lod = database_csv_retriever("infection_date",'date_local','cases','deaths')
-
-    # infection_date_name_db = db.Table('infection_date', db.metadata, autoload = True, autoload_with = db.engine)
-    # infection_date_query = db.session.query(infection_date_name_db).filter(infection_date_name_db.c.date_local == infection_date_single_date).all()
-    # infection_date_date_df = pd.DataFrame(infection_date_query, columns = ['date_local','cases','deaths'])
-    # infection_date_date_df = infection_date_date_df.reset_index(drop=True)
-    # infection_date_date_df['date_local'] = infection_date_date_df['date_local'].astype(str)
-    # infection_date_date_lod = infection_date_date_df.to_dict('records')
-    
     
     return jsonify(infection_date_lod)
     # return jsonify(infection_date_date_lod)
@@ -189,12 +114,10 @@ def air_line():
 def stocks():
 
     stock_data = get_data.main()
-    
     stock_data_df = pd.DataFrame(stock_data)
     stock_data_df.drop(index = 0, inplace = True)
     stock_data_df['date'] = stock_data_df['date'].astype(str)
     stock_data_df=stock_data_df.rename(columns = {'date':'date_local'})
-    
     stock_data_lod = stock_data_df.to_dict('records')
     
     # return render_template('index2.html', stock_data = stock_data)
